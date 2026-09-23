3f5f6353326a61f87aa37df7c04122e735cc0218
Nadazh92 <nadalizaher92@gmail.com>
2026-09-23 09:14:08 +0200

    finish LB project

--- js/main.js
+++ js/main.js
@@ -226,12 +226,13 @@ class Contact extends Page {
 
 
 class ProjectCard {
-    constructor(src, alt, title, link,lHref, fBtn, fBtnhref, sBtn, sBtnhref){
+    constructor(src, alt, title, link,lHref,lID, fBtn, fBtnhref, sBtn, sBtnhref){
         this.src =src;
         this.alt=alt;
         this.title=title;
         this.link=link;
         this.lHref= lHref;
+        this.lID=lID;
         this.fBtn=fBtn;
         this.sBtn=sBtn;
         this.fBtnhref =fBtnhref;
@@ -250,6 +251,7 @@ class ProjectCard {
         const readMore = document.createElement("a");
         readMore.textContent = this.link;
         readMore.href=this.lHref;
+        readMore.id=this.lID;
 
         const btnContainer =document.createElement("div");
         
@@ -299,6 +301,60 @@ class Projects {
         return section;
  
     }
+}
+
+class Cases {
+    constructor(img,client,year,duration,Deliverables,Challenges, Solutions, Results,Tools, flink,flinkName, slink,slinkName, image1,image2,image3,image4){
+        this.img=img;
+        this.client=client;
+        this.year=year;
+        this.duration=duration;
+        this.Deliverables=Deliverables;
+        this.Challenges=Challenges;
+        this.Solutions=Solutions;
+        this.Results=Results;
+        this.Tools=Tools;
+        this.flink=flink;
+        this.flinkName=flinkName;
+        this.slink=slink;
+        this.slinkName=slinkName;
+        this.image1=image1;
+        this.image2=image2;
+        this.image3=image3;
+        this.image4=image4;
+    }
+
+    render(){
+        return `
+        <section class="case-study">
+            <img src="${this.img}" alt="case image">
+            <div class="case-info">
+            <p>Client: ${this.client}</p>
+            <p>Year: ${this.year}</p>
+            <p>Duration: ${this.duration}</p>
+            </div>
+            <span>Deliverables</span><p> ${this.Deliverables}</p>
+            <span>Challenges </span><p>${this.Challenges}</p>
+            <span>Solutions</span><p> ${this.Solutions}</p>
+            <span>Results</span><p>${this.Results}</p>
+            <span>Tools & Technologies </span><p> ${this.Tools}</p>
+            <div class="links-container">
+            <a href=${this.flink}>${this.flinkName}</a>
+            <a href=${this.slink}>${this.slinkName}</a>
+            </div>
+            <div class="img-gallery">
+            <img src="${this.image1}" alt="case image 1">
+            <img src=${this.image2} alt="case image 2">
+            <img src="${this.image3}" alt="case image 3">
+            <img src=${this.image4} alt="case image 3">
+            </div>
+
+        </section>
+        `
+        
+    }
+
+
 }
     
 
@@ -361,6 +417,7 @@ document.body.addEventListener("click",function(event){
                  "Lady Balance", 
                  "Read more about the case",
                  "#",
+                 "read-more-lb",
                  "Live Website", 
                  "http://nadazh.dk/LB/", 
                  "Source Code", 
@@ -372,6 +429,7 @@ document.body.addEventListener("click",function(event){
                  "Garn & Craft", 
                  "Read more about the case",
                  "#",
+                 "read-more-gg",
                  "Live Website", 
                  "http://nadazh.dk/LB/", 
                  "Source Code", 
@@ -383,6 +441,7 @@ document.body.addEventListener("click",function(event){
                  "GLS AI PlayBook", 
                  "Read more about the case",
                  "#",
+                 "read-more-gls",
                  "Prototypen", 
                  "https://tinyurl.com/dwjt8pe9", 
                  "Download filen", 
@@ -397,10 +456,77 @@ document.body.addEventListener("click",function(event){
 
          const projectContent = document.querySelector("#project-content");
          projectContent.append(pro.render());
-   
+
+ 
 }
-}
-);
+
+
+         if(event.target.closest("#read-more-lb")){
+         console.log("iam her");
+         event.preventDefault(); 
+         document.body.classList.remove("home-page");
+         const lbCase = new Page ("Lady Balance case study");
+
+         const ladybalanceCase = new Cases("img/LB.png",
+         "Lady Balance",
+         "2026",
+         " 4 weeks",
+         "Landing page redesign, visual identity and logo redesign, design guide, campaign content, email automation flow, and SEO-focused content.",
+         "Lady Balance's existing digital presence lacked visual consistency, clear communication, and a user-friendly structure that reflected the quality and credibility of its products.",
+         "I used Design Thinking, UX research, and visual design principles to create a more coherent digital experience. The solution combined a redesigned visual identity, an accessible and responsive landing page, SEO-focused content, and a customer journey supported by campaign and email automation concepts",
+         "A more professional, trustworthy, and user-friendly digital concept that strengthens brand recognition, improves the customer journey, and creates better opportunities for engagement and customer loyalty.",
+         "Figma - HTML - CSS & SCSS - javaScript - SEO Tools -  Adobe Illustrator, Indesign, After Effect, Premiere Pro. ",
+         "https://indd.adobe.com/view/642a4652-42c9-4cb2-bf50-a277dded91ff",
+         "https://tinyurl.com/4m7nndbk",
+         "img/LB-image1.png",
+         "img/LB-image2.png",
+         "img/LB-image3.png",
+         "img/LB-image4.png"
+         );
+
+          document.body.innerHTML = lbCase.getNav() +  ` <main id="case-content"></main>` + lbCase.getFooter();
+          const caseContent = document.querySelector("#case-content");
+          caseContent.innerHTML = ladybalanceCase.render();
+
+        }
+
+
+           if(event.target.closest("#read-more-gg")){
+      
+         event.preventDefault(); 
+         document.body.classList.remove("home-page");
+         const ggCase = new Page ("Garn and Craft case study");
+
+         const garnandcraftCase = new Cases("img/project2.png",
+         "Garn & Craft",
+         "2025",
+         " 4 weeks",
+         "UX/UI prototype in Figma, responsive HTML/CSS website, user research, competitor analysis and visual design concept.",
+         "Expanding Garn & Craft's target audience while creating a modern, inclusive and user-friendly digital experience.",
+         "Applied Design Thinking, user research and usability testing to develop a responsive website with clear navigation, accessible design and a stronger focus on creativity and community.",
+         "Created a modern website concept that improved visual clarity and usability, providing a foundation for a stronger digital presence and broader audience engagement.",
+         "Figma - HTML - CSS - Adobe Photoshop ",
+         "https://indd.adobe.com/view/642a4652-42c9-4cb2-bf50-a277dded91ff",
+         "https://tinyurl.com/4m7nndbk",
+         "img/me2.png",
+         "img/me2.png"
+         );
+
+    
+
+
+          document.body.innerHTML = ggCase.getNav() +  ` <main id="case-content"></main>` + ggCase.getFooter();
+          const caseContent = document.querySelector("#case-content");
+          caseContent.innerHTML = garnandcraftCase.render();
+
+        }
+
+
+
+
+
+});
+
 
 
 
